561c2b20f996d9d74be97b90799736b4a8836dec
eridan123 <saryguloveridan@gmail.com>
2020-01-12 02:09:11 +0600

    to postgresql

--- ustim/settings.py
+++ ustim/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'ustim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'eri',
+        'PASSWORD': 'idinahui',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
